647361b2dfd2eedc02a416f116455641600f10a3
mel-lpz <mel.lopez@etu.unilim.fr>
2026-01-21 16:28:28 +0100

    Test 3D V2 1

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 AFRAME.registerComponent('terminal-interaction', {
     init: function() {
-        console.log('Component terminal-interaction initialisé');
+        console.log('Component terminal-interaction initialisé sur:', this.el);
         
-        this.el.addEventListener('click', () => {
-            console.log('Clic détecté sur le terminal!');
-            const grass = document.querySelector('#grass');
+        const grass = document.querySelector('#grass');
+        
+        // Fonction pour faire apparaître la plante
+        const showGrass = (evt) => {
+            console.log('Interaction détectée!', evt.type);
             if (grass) {
                 grass.setAttribute('scale', '1 1 1');
-                console.log('Grass scale changé à 1 1 1');
+                console.log('Grass visible!');
+            } else {
+                console.error('Grass non trouvé!');
             }
+        };
+        
+        // Desktop: clic avec la souris via le cursor
+        this.el.addEventListener('click', showGrass);
+        
+        // VR: événements des contrôleurs
+        this.el.addEventListener('mouseenter', () => {
+            console.log('Curseur sur le terminal');
+        });
+        
+        this.el.addEventListener('mouseleave', () => {
+            console.log('Curseur quitte le terminal');
         });
     }
 });
