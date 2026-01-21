a29c49b95e393db9f4949bd3ccad4173e3c2a6a1
mel-lpz <mel.lopez@etu.unilim.fr>
2026-01-21 16:08:37 +0100

    Test 3D 4

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Script chargé');
-    
-    const terminal = document.querySelector('#terminalTrigger');
-    const grass = document.querySelector('#grass');
-    
-    console.log('Terminal trouvé:', terminal);
-    console.log('Grass trouvé:', grass);
-
-    if (terminal && grass) {
-        terminal.addEventListener('click', () => {
+AFRAME.registerComponent('terminal-interaction', {
+    init: function() {
+        console.log('Component terminal-interaction initialisé');
+        
+        this.el.addEventListener('click', () => {
             console.log('Clic détecté sur le terminal!');
-            grass.setAttribute('scale', '1 1 1');
-            console.log('Grass scale changé à 1 1 1');
+            const grass = document.querySelector('#grass');
+            if (grass) {
+                grass.setAttribute('scale', '1 1 1');
+                console.log('Grass scale changé à 1 1 1');
+            }
         });
-    } else {
-        console.error('Éléments non trouvés');
     }
 });
